ccb0776e0b0448dba73cd7d0aff13bdd305a63bb
Mohamed Nasser <nasseratic@gmail.com>
2023-08-06 01:39:51 +0200

    current

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -82,20 +82,20 @@ const StudentItem = ({
             </p>
           </div>
         </div>
-        {session?.isTeacher && (
+        {session?.isTeacher && isCurrentStudent && (
           <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
             <button
               type="button"
-              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              className="focus:outline-none text-white font-medium rounded-lg text-2xl p-2"
             >
-              تم
+              ✅
             </button>
 
             <button
               type="button"
-              className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              className="focus:outline-none text-white font-medium rounded-lg text-2xl  p-2"
             >
-              غياب
+              ❌
             </button>
           </div>
         )}
